22a005f43bbede0e0d145e5f8f1961a3f535fdb3
Danny <d.khorasanizadeh@ec.iut.ac.ir>
2021-04-11 23:08:38 +0430

    Added Models

---
+++ blog/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.core.validators import MinLengthValidator
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    excerpt = models.CharField(max_length=200)
+    date = models.DateField(auto_now=True)
+    author = models.CharField(max_length=200)
+    content = models.TextField(validators=[MinLengthValidator(10)])
+
+
+class Comment(models.Model):
+    text = models.TextField(validators=[MinLengthValidator(10)])
+    author = models.CharField(max_length=200)
+    email = models.EmailField(max_length=254)
+    post = models.models.ForeignKey(Post, on_delete=models.CASCADE)
