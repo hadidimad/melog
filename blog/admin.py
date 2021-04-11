aaba392609648a68f96a66ac48de0d94c6fb75ee
Danny <d.khorasanizadeh@ec.iut.ac.ir>
2021-04-11 23:48:32 +0430

    Added models to admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Post, Comment
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_filter = ("author", "date")
+    list_display = ("title", "author", "date")
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("author", "email", "date_time")
+    list_filter = ("date_time", "author")
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
